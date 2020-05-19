9d7f973e95d2274ca1f4b909d3eecea542881a4c
LepuzFCoder <emirhan.durusoy@gmail.com>
2020-05-19 20:11:22 +0300

    Validation error yakalanması tamamlandı

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,13 +1,36 @@
+const developmentError = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+    stack: err.stack,
+    err,
+  });
+};
+
+const productionError = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+};
+
+const validationError = (err, res) => {
+  const errors = err.message.split(':');
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.errors,
+  });
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status =
     err.status || err.statusCode.toString().startsWith('4') ? 'fail' : 'error';
 
   if (process.env.NODE_ENV === 'development') {
-    res.status(err.statusCode).json({
-      status: err.status,
-      message: err.message,
-      stack: err.stack,
-    });
+    return developmentError(err, res);
   }
+
+  if (err.name === 'ValidationError') validationError(err, res);
 };
