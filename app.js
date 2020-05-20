403ba14c6f15b583cff9c9ba9be42c1148985211
LepuzFCoder <emirhan.durusoy@gmail.com>
2020-05-20 16:20:15 +0300

    Güvenlik önlemleri tamamlandı

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimiter = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const errorController = require('./controllers/errorController');
 const AppError = require('./utils/appError');
@@ -17,13 +21,16 @@ const app = express();
 app.use(express.json());
 
 //GÜVENLİK ÖNLEMLERİ:
-
 const limiter = rateLimiter({
   max: 500,
   windowMs: 1000 * 60 * 30,
 });
 
 app.use('/api', limiter);
+app.use(helmet());
+app.use(mongoSanitize());
+app.use(xss());
+app.use(hpp());
 
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
 
