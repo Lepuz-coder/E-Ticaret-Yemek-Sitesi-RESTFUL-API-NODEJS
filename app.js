d3c739082e6c651a0e113a6f9e2f96c7f0ea15b7
LepuzFCoder <emirhan.durusoy@gmail.com>
2020-05-20 16:38:20 +0300

    Ürünler template temeli oturtuldu

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const path = require('path');
 const morgan = require('morgan');
 const rateLimiter = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
+const cookieParser = require('cookie-parser');
 
 const errorController = require('./controllers/errorController');
 const AppError = require('./utils/appError');
@@ -18,7 +20,8 @@ const begenRouter = require('./routes/begenRoutes');
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json({ limit: '10kb' }));
+app.use(cookieParser());
 
 //GÜVENLİK ÖNLEMLERİ:
 const limiter = rateLimiter({
@@ -31,6 +34,10 @@ app.use(helmet());
 app.use(mongoSanitize());
 app.use(xss());
 app.use(hpp());
+
+//Render işlemleri:
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
 
