7c86c1f929d138c4b18ac561bc53376f785ac883
LepuzFCoder <emirhan.durusoy@gmail.com>
2020-05-23 14:20:45 +0300

    Beğenme alert boxu tamamlandı

--- public/js/models/urunlerModel.js
+++ public/js/models/urunlerModel.js
@@ -1,18 +1,32 @@
 /* eslint-disable */
 import '@babel/polyfill';
 import axios from 'axios';
+import Swal from 'sweetalert2';
 
 export const begenToggle = async (id) => {
   try {
+    Swal.fire({
+      title: 'Yükleniyor...',
+      timer: '1000',
+      imageUrl: '/gif/loading.gif',
+      imageWidth: 100,
+      showConfirmButton: false,
+    });
     const res = await axios({
       method: 'POST',
       url: `/api/v1/begen/toggle/${id}`,
     });
 
     if (res.data) {
-      alert('eklendi');
+      Swal.fire({
+        icon: 'success',
+        title: 'Beğeni eklendi',
+      });
     } else {
-      alert('cikarildi');
+      Swal.fire({
+        icon: 'error',
+        title: 'Beğeni kaldırıldı',
+      });
     }
   } catch (err) {
     console.log(err.response.data);
